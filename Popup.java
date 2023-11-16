4354916bc1af1c40e6ea6af92f25499fa4b9ed9a
ashmcd <jaidening340@gmail.com>
2023-11-16 12:04:59 -0500

    popup stuff
    i suck at coding

---
+++ Popup.java
@@ -0,0 +1,17 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Popup here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Popup extends Actor
+{
+  
+    
+    public Popup() {
+        
+    }
+    
+}
