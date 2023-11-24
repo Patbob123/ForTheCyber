6225459b22bf8d6388cadf9e17258dc39f0eb86e
Mangofish369 <117941655+Mangofish369@users.noreply.github.com>
2023-11-23 22:41:20 -0500

    3 waves + boss
    - Missing a start label on the button

--- EnemyWorld.java
+++ EnemyWorld.java
@@ -28,6 +28,7 @@ public class EnemyWorld extends World
     private int enemyPerWave;
     private HashMap<Integer, Enemy> enemyDict;
     private ArrayList<ArrayList<Enemy>> waves;
+    private GreenfootImage title;
 
     public EnemyWorld()
     {    
@@ -41,37 +42,54 @@ public class EnemyWorld extends World
             put(3,new Juggernaut());
             put(4,new Mech57());
             put(5,new SkeleCop());
+            put(6, new Boss());
         }};
 
         waves = new ArrayList<ArrayList<Enemy>>();
         enemyPerWave = 10;
         generateEnemies(0);
         generateEnemies(1);
+        generateEnemies(2);
+        generateEnemies(3);
         
         Presser nextButton = new Presser(goBuilderWorld, "ready.png", "ready.png");
-        addObject(nextButton, 500, 700);
+        addObject(nextButton, 800, 700);
         
-        displayEnemies(getStages().get(0), 100);
-        displayEnemies(getStages().get(1), 200);
+        title = new GreenfootImage("enemyWaveText.png");
+        displayEnemies(getStages().get(0), 200);
+        displayEnemies(getStages().get(1), 350);
+        displayEnemies(getStages().get(2), 500);    
+        displayEnemies(getStages().get(3), 650);
+        setBackground(title); 
+        
     }
     public void generateEnemies(int difficulty){
-        ArrayList <Enemy> stageEnemies = new ArrayList <Enemy>();
+        ArrayList <Enemy> waveEnemies = new ArrayList <Enemy>();
         int random;
         if (difficulty == 0){ // Tier 1 enemies
-            int numOfHardEnemies = 2;
             
             for(int i = 0; i < enemyPerWave; i++){
                 random = Greenfoot.getRandomNumber(3);
-                stageEnemies.add(enemyDict.get(random));
+                waveEnemies.add(enemyDict.get(random));
             }
 
         } else if (difficulty == 1){ // Tier 2 enemies
             for(int i = 0; i < enemyPerWave; i++){
-                random = Greenfoot.getRandomNumber(3);
-                stageEnemies.add(enemyDict.get(random+3));
+                random = Greenfoot.getRandomNumber(6);
+                waveEnemies.add(enemyDict.get(random));
             }
+        } else if (difficulty == 2){
+            for(int i = 0; i < enemyPerWave; i++){
+                random = Greenfoot.getRandomNumber(3);
+                waveEnemies.add(enemyDict.get(random+3));
+            }
+        } else if (difficulty == 3){
+           enemyPerWave = 1; 
+           for(int i = 0; i < enemyPerWave; i++){
+                waveEnemies.add(enemyDict.get(6));
+            } 
         }
-        waves.add(stageEnemies);
+        waves.add(waveEnemies);
     }
     public ArrayList<ArrayList<Enemy>> getStages(){
         return waves;
@@ -83,7 +101,7 @@ public class EnemyWorld extends World
         for (int i = 0; i< wave.size(); i++){
             GreenfootImage img = wave.get(i).getPortrait();
             UI portrait = new UI(img,false);
-            addObject(portrait, (i*50)+300, height);
+            addObject(portrait, (i*50)+450, height);
         }   
     }
 
