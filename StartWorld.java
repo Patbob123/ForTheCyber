904bba30fa567973592fdad4ea6af9d2749e3861
ilovecats623 <139278403+ilovecats623@users.noreply.github.com>
2023-11-25 00:16:28 -0500

    startworld update #28467

--- StartWorld.java
+++ StartWorld.java
@@ -1,101 +1,119 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Simulation start screen with music 
+ * Simulation start screen with music, animated Logo, animated bg, and faders
+ * <p>
+ * Click to proceed
  * 
  * @author Edmond 
- * @version nov2023
+ * @version 2023 Nov 24
  */
 public class StartWorld extends SuperWorld
 {
     private int acts, currActs;
     private boolean mouseIsClicked, playLoopedAnim;
+    
     private static GifImage startBg;
     private GreenfootImage bgImage, transparentBg, whiteBg;
     private GreenfootImage logoF1, logoF2, logoF3;
     private GreenfootImage currImg;
     private Fader fade, fadeOut;
+    
     /**
-     * Constructor for objects of class StartWorld.
-     * 
+     * Constructor for objects of class StartWorld
      */
     public StartWorld()
     {
-        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(Constants.WORLD_WIDTH, Constants.WORLD_HEIGHT, 1);  
-        
-        startBg = new GifImage ("startBg.gif"); //add this in when added startBg
+
+        //Making new background images
+        startBg = new GifImage ("startBg.gif"); 
         transparentBg = new GreenfootImage ("transparentBg.png");
+        setBackground (transparentBg);
+
+        //Making frames of logo animation
         logoF1 = new GreenfootImage("logof1.png");
         logoF2 = new GreenfootImage("logof2.png");
         logoF3 = new GreenfootImage("logof3.png");
         whiteBg = new GreenfootImage("whiteBg.png");
         bgImage = startBg.getCurrentImage();
-        
-        setBackground (transparentBg);
+
+        //faders
         fade = new Fader ((60*4), false); //60 acts = 1 second, so 4 seconds for fader
         fadeOut = new Fader ((60*2), true);
+
+        //setting initial values for variables
         playLoopedAnim = true;
         mouseIsClicked = false;
         currActs = 0;
+
+        //add fader object to fade in on creation
         addObject(fade, Constants.WORLD_WIDTH/2, Constants.WORLD_HEIGHT/2);
 
     }
     
     public void act (){
-        
+        //keep track of acts, animate bg
         acts++;
-        
+        animateBg();
+
         if(acts==1){
+            //this instead of constructor so the music doesnt play before simulation is run
             sm.playSoundLoop("Jaded");
             sm.fadeIn("Jaded");
-            
         }
         
         if (acts == 10){
+            //play rain ambience
             sm.playSoundLoop("rain");
         }
         
-        if(acts > 240 && Greenfoot.mouseClicked(null)){ //if mouse click and more than 4 seconds
+        if(acts > 240 && Greenfoot.mouseClicked(null)){ //if mouse clicked and more than 4 seconds
+            //tells everyone that the mouse has been clicked, so get ready to switch worlds
             mouseIsClicked = true;
+            
+            //click sound
             sm.playSound("blip");
+            
+            //fade out music + ambience
             sm.fadeOut("Jaded");
             sm.fadeOut("rain");
         }
         
         if(mouseIsClicked){
+            //add fade out object
             addObject(fadeOut, Constants.WORLD_WIDTH/2, Constants.WORLD_HEIGHT/2);
+            
+            //mark how many acts has passed
             currActs++;
+            
             if (currActs >= fadeOut.getMaxDuration()){
+                //when fader is done, stop sounds and switch worlds
                 sm.stopSounds();
                 Greenfoot.setWorld(new IntroWorld());
             }
         }
         
-        //playLogoAnim();
-        currImg = getLogoImg();
-        bgImage = startBg.getCurrentImage();
-        transparentBg.drawImage(bgImage, 0, 0);
-        
+        //logo "glitches" out a lot at beginnning, but becomes fairly solid as time passes
         if(playLoopedAnim){
             if(acts < (60*2)){
-                if(Greenfoot.getRandomNumber(20) > 1){
+                if(randNum(20) > 1){
                         transparentBg.drawImage(currImg, 0, 0);
                 }
             } else if (acts < (60*5)){
-                if(Greenfoot.getRandomNumber(50) > 1){
+                if(randNum(50) > 1){
                         transparentBg.drawImage(currImg, 0, 0);
                 }
             } else if (acts < (60*8)){
-                if(Greenfoot.getRandomNumber(80) > 1){
+                if(randNum(80) > 1){
                         transparentBg.drawImage(currImg, 0, 0);
                 }
             } else {
-                if(Greenfoot.getRandomNumber(120) > 1){
+                if(randNum(120) > 1){
                         transparentBg.drawImage(currImg, 0, 0);
                 }
             }
-        } //draw logo img ontop
+        } 
         
         if(acts >= (60*60)){ //if wait 1 min, easter egg (logo dies)
             playLoopedAnim = false;
@@ -103,19 +121,27 @@ public class StartWorld extends SuperWorld
 
     }
     
-    private GreenfootImage getLogoImg(){
-        if (acts % 3 == 0){
-            return(logoF1);
-        }
-        if (acts % 3 == 1){
-            return(logoF2);
-        }
-        if (acts % 3 == 2){
-            return(logoF3);
-        }
-        return(logoF1);
+    private void animateBg(){
+        currImg = getLogoImg();
+        bgImage = startBg.getCurrentImage();
+        transparentBg.drawImage(bgImage, 0, 0);
     }
     
+    private GreenfootImage getLogoImg(){
+        //return current frame of logo animation
+        if (acts % 3 == 0){
+            return logoF1;
+        }
+        if (acts % 3 == 1){
+            return logoF2;
+        }
+        if (acts % 3 == 2){
+            return logoF3;
+        }
+        return logoF1;
+    }
     
-    
+    private int randNum(int max){
+        return Greenfoot.getRandomNumber(max);
+    }
 }
